423666558f54d225ac4ec2e69af292cb66f38324
Gabriel das Neves <galvesdasneves@gmail.com>
2022-09-16 15:06:12 -0300

    heading + logo in home page

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,9 +1,14 @@
-import {View} from 'react-native';
+import {View, Image} from 'react-native';
 import {styles} from './styles';
+import logoImg from '../../assets/games/logo-nlw-esports.png';
+import { Heading } from '../../components/Heading';
 
 export function Home(){
     return(
         <View style={styles.container}>
+            <Image source={logoImg} style={styles.logo} />
+
+            <Heading title="Encontre o seu duo" subtitle="Seleciona o game que deseja jogar"/>
         </View>
     );
 }
