f2f8d27d3bddcb561701859afcde097befa09bb4
Gabriel das Neves <galvesdasneves@gmail.com>
2022-09-17 14:34:20 -0300

    database models and relationships done

--- SERVER/src/server.ts
+++ SERVER/src/server.ts
@@ -1,14 +1,29 @@
-import express from 'express'
+import express, { request, response } from 'express'
 
-const app = express()
+const app = express();
 
-app.get('/ads', (request, response) => {
+app.get('/games', (request,response) => {
+    return response.json([]);
+})
+
+app.post('/ads', (request,response) => {
+    return response.status(201).json([]);
+})
+
+app.get('/games/:id/ads', (request, response) => {
+    // const gameId = request.params.id;
+
     return response.json([
         {id:1, name:'anuncio1'},
         {id:2, name:'anuncio2'},
         {id:3, name:'anuncio3'},
         {id:4, name:'anuncio4'},
     ])
+})
+
+app.get('/games/:id/discord', (request, response) => {
+    // const adId = request.params.id;
+    return response.json([])
 })
 
 app.listen(3333)
