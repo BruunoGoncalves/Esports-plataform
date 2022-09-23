8b7bc6dca00387c58c609b10ef497944f7a8bc5f
Gabriel das Neves <galvesdasneves@gmail.com>
2022-09-22 22:37:47 -0300

    fetching api and routes

---
+++ mobile/App.tsx
@@ -0,0 +1,23 @@
+import { StatusBar } from 'react-native';
+import { Background }from './src/components/Background';
+import { useFonts, Inter_400Regular,Inter_600SemiBold, Inter_900Black, Inter_700Bold} from '@expo-google-fonts/inter';
+import {Routes} from './src/routes';
+import {Loading} from './src/components/Loading';
+
+export default function App() {
+
+  const [fontsLoaded] = useFonts({Inter_400Regular,Inter_600SemiBold, Inter_900Black, Inter_700Bold});
+
+  return (
+    <>
+      <Background >
+        <StatusBar barStyle='light-content' backgroundColor='transparent' translucent />
+        {fontsLoaded ? <Routes /> : <Loading />}
+      </Background>
+    </>
+  );
+}
+
+
+
+
---
+++ mobile/src/screens/Home/index.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Image, FlatList} from 'react-native';
+import {styles} from './styles';
+import logoImg from '../../assets/logo-nlw-esports.png';
+import { Heading } from '../../components/Heading';
+import { GameCard, GameCardprops } from '../../components/GameCard';
+import {useEffect, useState} from 'react'
+import {SafeAreaView} from 'react-native-safe-area-context'
+import { Background } from '../../components/Background';
+import {useNavigation} from '@react-navigation/native'
+
+export function Home(){
+    const [games, setGames] = useState<GameCardprops[]>([]);
+
+    const navigation = useNavigation();
+
+    function handleOpenGame({id, title, bannerUrl}: GameCardprops){
+        navigation.navigate('game',{id, title, bannerUrl});
+    }
+
+    useEffect(() => {
+        fetch('http://192.168.0.8:3333/games')
+        .then(response => response.json())
+        .then(data => setGames(data))
+    },[])
+
+    return(
+        <Background>
+        <SafeAreaView style={styles.container}>
+            <Image source={logoImg}
+                    style={styles.logo}
+            />
+
+            <Heading title="Encontre o seu duo" 
+                    subtitle="Seleciona o game que deseja jogar"
+            />
+
+            <FlatList data={games} 
+                        keyExtractor={item => item.id} 
+                        renderItem={({item}) => (
+                                                    <GameCard data={item} onPress={() => handleOpenGame(item)} />
+                                                )
+                                    } 
+                        horizontal showsVerticalScrollIndicator={false}
+            />
+            
+        </SafeAreaView>
+        </Background>
+    );
+}
